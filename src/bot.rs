05547fdf95dc58f96f78d91155ef0c36db066e65
Nelson Lee <leelhn2345@outlook.com>
2023-09-11 00:52:19 +0800

    send msg anonymously

--- src/bot.rs
+++ src/bot.rs
@@ -11,26 +11,27 @@ use teloxide::types::UserId;
 use teloxide::types::{Message, Update};
 use teloxide::{update_listeners::UpdateListener, Bot};
 
+fn check_is_owner(msg: Message, owners: &HashSet<u64>) -> bool {
+    let user = msg.from();
+    let Some(user_id) = user else { return false };
+    let UserId(id) = user_id.id;
+    owners.contains(&id)
+}
 pub async fn start_bot(
     bot: Bot,
     listener: impl UpdateListener<Err = Infallible>,
     settings: Settings,
 ) {
-    let owners = HashSet::from([2050440697, 220272763]);
+    let owners: HashSet<u64> = HashSet::from([2050440697, 220272763]);
 
     let handler = dptree::entry()
-        .inspect(|u: Update| println!("{:#?}", u))
+        // .inspect(|u: Update| println!("{:#?}", u))
         .branch(
             Update::filter_message()
                 // .filter(|msg: Message| msg.chat.id.is)
                 .branch(
                     teloxide::filter_command::<OwnerCommand, _>()
-                        .filter(move |msg: Message| {
-                            let user = msg.from();
-                            let Some(user_id) = user else { return false };
-                            let UserId(id) = user_id.id;
-                            owners.contains(&id)
-                        })
+                        .filter(move |msg: Message| check_is_owner(msg, &owners))
                         .endpoint(OwnerCommand::parse_commands),
                 )
                 .branch(
--- src/handlers/command.rs
+++ src/handlers/command.rs
@@ -3,10 +3,18 @@ use crate::{
     stickers::{send_many_stickers, send_sticker},
     types::MyResult,
 };
-use teloxide::{requests::Requester, types::Message, utils::command::BotCommands, Bot};
+use teloxide::{
+    requests::Requester,
+    types::{ChatId, Message},
+    utils::command::{BotCommands, ParseError},
+    Bot,
+};
 
 #[derive(BotCommands, Clone)]
-#[command(rename_rule = "lowercase", description = "hello owner!~ 😊")]
+#[command(
+    rename_rule = "lowercase",
+    description = "hello owner!~ 😊. these are the available commands"
+)]
 pub enum OwnerCommand {
     #[command(description = "list down all commands")]
     Help,
@@ -16,10 +24,14 @@ pub enum OwnerCommand {
     // Greet,
     #[command(description = "a passionate kiss")]
     Kiss,
+    #[command(description = "give you my LOVE")]
+    Love,
     #[command(description = "feed me!")]
     Feed,
     #[command(description = "IT'S PARTY TIME!! 🥳🥳")]
     Party,
+    #[command(parse_with = message_to_send)]
+    SendMessage(i64, String),
 }
 impl OwnerCommand {
     pub async fn parse_commands(
@@ -38,6 +50,10 @@ impl OwnerCommand {
             OwnerCommand::Party => {
                 send_many_stickers(&bot, &msg, settings.stickers.party_animals).await?
             }
+            OwnerCommand::Love => send_sticker(&bot, &msg, settings.stickers.love).await?,
+            OwnerCommand::SendMessage(chat_id, msg_string) => {
+                bot.send_message(ChatId(chat_id), msg_string).await?;
+            }
             _ => {
                 send_sticker(&bot, &msg, settings.stickers.coming_soon).await?;
                 bot.send_message(msg.chat.id, "~ feature coming soon ~")
@@ -48,10 +64,31 @@ impl OwnerCommand {
     }
 }
 
+fn message_to_send(input: String) -> Result<(i64, String), ParseError> {
+    let mut parts = input.splitn(2, ' ');
+
+    let chat_id = parts
+        .next()
+        .unwrap_or_default()
+        .parse::<i64>()
+        .map_err(|e| ParseError::IncorrectFormat(e.into()))?;
+
+    let message = parts.next().unwrap_or("yo yo yo").into();
+
+    Ok((chat_id, message))
+}
+
 #[derive(BotCommands, Clone)]
-#[command(rename_rule = "lowercase", description = "hello user 😊~")]
+#[command(
+    rename_rule = "lowercase",
+    description = "hello user 😊~. These are the available commands."
+)]
 pub enum UserCommand {
     Start,
+    #[command(description = "list down all commands")]
+    Help,
+    #[command(parse_with = message_to_send)]
+    SendMessage(i64, String),
 }
 
 impl UserCommand {
@@ -67,8 +104,15 @@ impl UserCommand {
                     Some(x) => format!("hello @{}! 🐢", x),
                     None => String::from("hello friend!"),
                 };
+                send_sticker(&bot, &msg, settings.stickers.hello).await?;
                 bot.send_message(msg.chat.id, text).await?;
-                send_sticker(&bot, &msg, settings.stickers.hello).await?
+            }
+            UserCommand::Help => {
+                bot.send_message(msg.chat.id, OwnerCommand::descriptions().to_string())
+                    .await?;
+            }
+            UserCommand::SendMessage(chat_id, msg_string) => {
+                bot.send_message(ChatId(chat_id), msg_string).await?;
             }
         }
         Ok(())
