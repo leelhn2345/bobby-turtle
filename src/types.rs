5ac45a305fb8d19f2730dab400105c03beaa0452
Nelson Lee <leelhn2345@outlook.com>
2023-09-13 18:51:46 +0800

    added possible error type

--- src/types.rs
+++ src/types.rs
@@ -2,6 +2,9 @@
 pub enum MyError {
     #[error(transparent)]
     RequestError(#[from] teloxide::RequestError),
+    // for `return Err("wtf is this error")`
+    // #[error("unknown error: {0}")]
+    // Unknown(String),
 }
 
 pub type MyResult<T> = Result<T, MyError>;
