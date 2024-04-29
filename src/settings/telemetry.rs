2f3f10bb744c0a85a37c862186f145a14466bced
Nelson Lee <leelhn2345@outlook.com>
2024-04-30 04:52:30 +0800

    chore: remove tracing envfilter

--- src/settings/telemetry.rs
+++ src/settings/telemetry.rs
@@ -1,28 +1,16 @@
 use tracing::level_filters::LevelFilter;
-use tracing_subscriber::filter::EnvFilter;
+use tracing_subscriber::filter::Targets;
 use tracing_subscriber::{fmt, layer::SubscriberExt};
 
 use crate::settings::environment::Environment;
 
 pub fn init_tracing(env: &Environment) {
-    let env_layer = match *env {
-        Environment::Local => EnvFilter::from_default_env()
-            .add_directive(
-                format!("telebot={}", LevelFilter::DEBUG)
-                    .parse()
-                    .expect("unable to set telebot tracing for local runtime"),
-            )
-            .add_directive(
-                format!("tower_http={}", LevelFilter::DEBUG)
-                    .parse()
-                    .expect("unable to set axum tracing middleware for local runtime"),
-            ),
-        Environment::Production => EnvFilter::from_default_env().add_directive(
-            format!("telebot={}", LevelFilter::INFO)
-                .parse()
-                .expect("unable to set telebot tracing for production runtime"),
-        ),
+    let env_level = match *env {
+        Environment::Local => LevelFilter::DEBUG,
+        Environment::Production => LevelFilter::INFO,
     };
+
+    let target_filter = Targets::new().with_target("telebot", env_level);
 
     let format_layer = fmt::layer()
         .without_time()
@@ -31,35 +19,11 @@ pub fn init_tracing(env: &Environment) {
         .with_target(false);
 
     let subscriber = tracing_subscriber::registry()
-        .with(env_layer)
-        .with(format_layer);
+        .with(format_layer)
+        .with(target_filter);
 
     tracing::subscriber::set_global_default(subscriber).expect("failed to set tracing subscriber");
 
     let env_name = env.as_str();
     tracing::info!("telebot app started in {env_name} environment!");
-}
-
-#[cfg(test)]
-mod test {
-
-    #[derive(Debug, thiserror::Error)]
-    enum TestError {
-        #[error("effwgrgw")]
-        HelloError(String),
-    }
-
-    fn hello() -> Result<(), TestError> {
-        Err(TestError::HelloError("evfefe2f".to_string()))
-    }
-    #[test]
-    fn error() {
-        match hello() {
-            Ok(_v) => println!("hello"),
-            Err(e) => println!("{e}"),
-        }
-        let _ = hello().map_err(|e| println!("{e}"));
-        hello().expect("csddc");
-        // hello().unwrap();
-    }
 }
